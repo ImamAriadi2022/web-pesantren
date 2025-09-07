993ae527c85eb0251f11b782baf1251cbbf92db5
Imam ariadi <gamerapin139@gmail.com>
2025-09-07 12:21:16 +0700

    update cross

---
+++ backend/api/jadwal/jadwal_clean.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
+require_once '../../config/database.php';
+
+// TODO: Implement jadwal_clean functionality
+echo json_encode([
+    'success' => false,
+    'message' => 'API endpoint belum diimplementasi'
+]);
+?>
--- backend/api/surat_izin/add_status_column.php
+++ backend/api/surat_izin/add_status_column.php
@@ -1,4 +1,13 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit(0);
+}
+
 require_once '../../config/database.php';
 
 try {
